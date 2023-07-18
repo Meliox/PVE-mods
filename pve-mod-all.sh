4b0c868ccfaee7738f47fed74bdd2c1eeca4b98a
rmm <rmm@tenforward.space>
2023-07-18 18:25:44 +0200

    Helper script for triggering all mod scripts

---
+++ pve-mod-all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This script's working directory
+SCRIPT_CWD="$(dirname "$(readlink -f "$0")")"
+
+MODS="pve-mod-gui-temp.sh pve-mod-nag-screen.sh"
+
+ACTION=${1:-install}
+for m in $MODS; do
+	"$SCRIPT_CWD/$m" $ACTION
+done
