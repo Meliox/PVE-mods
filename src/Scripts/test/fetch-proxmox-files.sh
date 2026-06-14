aa3e635df890bed467ab0372999f81c1749f368b
Meliox <na>
2026-06-14 10:32:19 +0200

    more dep3

--- src/Scripts/test/fetch-proxmox-files.sh
+++ src/Scripts/test/fetch-proxmox-files.sh
@@ -13,9 +13,9 @@ echo "deb [trusted=yes] http://download.proxmox.com/debian/pve $SUITE pve-no-sub
     | sudo tee /etc/apt/sources.list.d/pve-test.list >/dev/null
 sudo apt-get update -qq
 
-echo "[fetch] Downloading pve-manager, proxmox-widget-toolkit, pve-yew-mobile-gui, libjson-perl, libpve-common-perl, and libclone-perl..."
+echo "[fetch] Downloading pve-manager, proxmox-widget-toolkit, pve-yew-mobile-gui, libjson-perl, libpve-common-perl, libclone-perl, and libcommon-sense-perl..."
 cd "$WORKDIR"
-apt-get download pve-manager proxmox-widget-toolkit pve-yew-mobile-gui libjson-perl libpve-common-perl libclone-perl
+apt-get download pve-manager proxmox-widget-toolkit pve-yew-mobile-gui libjson-perl libpve-common-perl libclone-perl libcommon-sense-perl
 
 echo "[fetch] Extracting to system paths..."
 for deb in *.deb; do
