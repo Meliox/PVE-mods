ddf53315a7255b754a6a096a0517172a0a1f3f04
Meliox <na>
2026-08-08 19:36:48 +0200

    one more

--- src/Scripts/test/fetch-proxmox-files.sh
+++ src/Scripts/test/fetch-proxmox-files.sh
@@ -13,9 +13,9 @@ echo "deb [trusted=yes] http://download.proxmox.com/debian/pve $SUITE pve-no-sub
     | sudo tee /etc/apt/sources.list.d/pve-test.list >/dev/null
 sudo apt-get update -qq
 
-echo "[fetch] Downloading pve-manager, proxmox-widget-toolkit, pve-yew-mobile-gui, libjson-perl, libpve-common-perl, libclone-perl, liblinux-inotify2-perl, libcommon-sense-perl, libhttp-message-perl and libstring-shellquote-perl..."
+echo "[fetch] Downloading pve-manager, proxmox-widget-toolkit, pve-yew-mobile-gui, libjson-perl, libpve-common-perl, libclone-perl, liblinux-inotify2-perl, libcommon-sense-perl, libhttp-message-perl, libstring-shellquote-perl and libencode-locale-perl..."
 cd "$WORKDIR"
-apt-get download pve-manager proxmox-widget-toolkit pve-yew-mobile-gui libjson-perl libpve-common-perl libclone-perl liblinux-inotify2-perl libcommon-sense-perl libhttp-message-perl libstring-shellquote-perl
+apt-get download pve-manager proxmox-widget-toolkit pve-yew-mobile-gui libjson-perl libpve-common-perl libclone-perl liblinux-inotify2-perl libcommon-sense-perl libhttp-message-perl libstring-shellquote-perl libencode-locale-perl
 
 echo "[fetch] Extracting to system paths..."
 for deb in *.deb; do
