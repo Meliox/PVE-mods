b91f7e5637a4b1077c873d744c95e0e645f818f0
Meliox <na>
2026-09-24 20:43:54 +0200

    remove whitespace

--- src/modules/node_info/files/PveMod_pvemanagerlib.js
+++ src/modules/node_info/files/PveMod_pvemanagerlib.js
@@ -544,7 +544,6 @@ Ext.define('PVE.node.StatusView', {
 				} catch(e) {
 					objValue = {};
 				}
-
 				const tempHelper = Ext.create('PVE.mod.TempHelper', {srcUnit: PVE.mod.TempHelper.CELSIUS, dstUnit: value.temp_unit === 'F' ? PVE.mod.TempHelper.FAHRENHEIT : PVE.mod.TempHelper.CELSIUS});
 				const ignoreThreshold = tempHelper.getTemp(parseFloat(value.ignore_temp_below));
 				const dimmKeys = Object.keys(objValue).filter(item => /^DIMM\d+$/.test(item)).sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }));
