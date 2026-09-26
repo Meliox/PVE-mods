b2ed0b8b4c8b81137584f1fe35c466e8f561169f
Meliox <na>
2026-09-26 10:12:15 +0200

    fix

--- src/modules/node_info/files/PveMods_pvemanagerlib.js
+++ src/modules/node_info/files/PveMods_pvemanagerlib.js
@@ -481,7 +481,7 @@ Ext.define('PVE.node.StatusView', {
                         // Temperature
                         if (stats.temperature) {
                             const gpuTemp = gpuTempHelper.getTemp(parseFloat(stats.temperature.gpu));
-                            const gpuIgnoreThreshold = gpuTempHelper.getTemp(parseFloat(gpuStats.ignore_temp_below));
+                            const gpuIgnoreThreshold = gpuTempHelper.getTemp(parseFloat(gpuInfo.ignore_temp_below));
                             if (gpuTemp >= gpuIgnoreThreshold) {
                                 const tempUnit = gpuTempHelper.getUnit();
                                 // Convert thresholds to target unit for comparison
